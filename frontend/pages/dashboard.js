7cfbf5d17c194b15e099d7d25353f1ad77ba04d0
newrduenas14 <newrduenas14@gmail.com>
2026-06-15 16:04:53 -0500

    Load phonefix2 dashboard API module

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,4 +1,4 @@
-import { getDashboard, resetToSpreadsheetSeed } from "../js/api.js?v=phonefix1";
+import { getDashboard, resetToSpreadsheetSeed } from "../js/api.js?v=phonefix2";
 import { can } from "../js/permissions.js";
 import { notice } from "../js/utils.js";
 
