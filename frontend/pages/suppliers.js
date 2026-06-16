d3f393061ca076619b971d0261a841b2a900d358
newrduenas14 <newrduenas14@gmail.com>
2026-06-16 15:20:51 -0500

    Use cached API on suppliers

--- frontend/pages/suppliers.js
+++ frontend/pages/suppliers.js
@@ -1,4 +1,4 @@
-import { createSupplier, listSuppliers } from "../js/api.js?v=opsupdate1";
+import { createSupplier, listSuppliers } from "../js/api-smooth1.js";
 import { can } from "../js/permissions.js";
 import { formToObject, notice, status, table } from "../js/utils.js";
 
