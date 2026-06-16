60646857b69466b198f9b182720c11864ce47e36
newrduenas14 <newrduenas14@gmail.com>
2026-06-16 12:28:19 -0500

    Update GOOGLE_SCRIPT_WEB_APP_URL to new endpoint

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxvma3JZmRfG5JRqZ7e6xWMc3kxElnnhk1hZwUud0WNtA4rydlkr5S6iTDzGI13rQn0/exec";
+export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwllB2WJ-0y8dm-jcEIsAYNfFRl9s0rX3nc-CX_rz0xJR9YgKsM80QwKdUNJoqotKzI/exec";
