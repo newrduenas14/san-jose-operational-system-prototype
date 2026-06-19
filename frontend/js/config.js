c66c9dcce8cabd557c064ae3df68d8254751eca4
newrduenas14 <newrduenas14@gmail.com>
2026-06-19 14:46:41 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyvo7wGec4shX1EXH8nPTPZWqrau5PGBCyNu4k70uJoE5kscSM05BppPAgOEIT2tmOE/exec";
+export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyDBTPyjhZzhrDX6xRC9Citc6Zvrn2NFCV1JcOo6P2wr4hSXOPIN8JsoHcbc4uOAd47/exec";
